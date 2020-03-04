530c37e9263a9d6f8ef5e60d634b4eb2b5e33af5
Victor Martinez <knoopx@gmail.com>
2020-03-04 10:30:09 +0100

    refactor: simplified FavIcon

--- src/renderer/components/FavIcon.jsx
+++ src/renderer/components/FavIcon.jsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react"
 import { types, unprotect, onSnapshot } from "mobx-state-tree"
 import { observer } from "mobx-react"
 import { MdRssFeed } from "react-icons/md"
-import { useSpring, animated } from "react-spring"
 import { scrape } from "support/parse"
 import query from "domqs"
-import { flow, filter, map } from "lodash/fp"
 
 const Store = types.map(types.string)
 const cache = Store.create(JSON.parse(localStorage.faviconCache || "{}"))
@@ -28,18 +26,21 @@ const tryFetchImage = async (url) => {
 
 const match = query(["link[rel*='icon']@href"])
 
-const fetchIcon = async (src) => {
+const resolveIcons = async (src) => {
   cache.set(src, "")
 
   try {
     const doc = await scrape(src, "text/html")
     const matches = match(doc)
 
-    const links = new Set([...matches, new URL("favicon.ico", src).toString()])
+    const candidates = new Set([
+      ...matches,
+      new URL("favicon.ico", src).toString(),
+    ])
 
-    for (const link of links) {
-      if (await tryFetchImage(link)) {
-        cache.set(src, link)
+    for (const candidate of candidates) {
+      if (await tryFetchImage(candidate)) {
+        cache.set(src, candidate)
         break
       }
     }
@@ -48,45 +49,19 @@ const fetchIcon = async (src) => {
   }
 }
 
-const Placeholder = animated(MdRssFeed)
-
 export const FavIcon = observer(({ src, className, ...props }) => {
-  const [isLoaded, setLoaded] = useState(false)
-
   const { origin } = src ? new URL(src) : { origin: null }
   const iconSrc = cache.get(origin)
 
   useEffect(() => {
-    if (src) {
-      if (iconSrc) {
-        const img = new Image()
-        img.onload = () => void setLoaded(true)
-        img.src = iconSrc
-      } else {
-        fetchIcon(origin)
-      }
+    if (src && !iconSrc) {
+      resolveIcons(origin)
     }
-  }, [src])
-
-  const imgSpring = useSpring({
-    opacity: isLoaded ? 1 : 0,
-  })
+  }, [src, iconSrc])
 
   if (!iconSrc) {
-    return <Placeholder className={["h-4 w-4", className]} {...props} />
+    return <MdRssFeed className={["h-4 w-4", className]} {...props} />
   }
 
-  return (
-    <animated.img
-      {...props}
-      className={["h-4 w-4", className]}
-      // style={imgSpring}
-      src={iconSrc}
-      // src={`https://api.statvoo.com/favicon/?url=${origin}`}
-      // src={`https://www.google.com/s2/favicons?origin=${origin}`}
-      // src={`https://besticon-demo.herokuapp.com/icon?size=16..${size}..16&url=${origin}`}
-      // src={`https://api.faviconkit.com/${origin}/${size}`}
-      // src={`https://f1.allesedv.com/${origin}`}
-    />
-  )
+  return <img {...props} className={["h-4 w-4", className]} src={iconSrc} />
 })
