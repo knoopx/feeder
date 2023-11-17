c3d77e5c7c69efb9827d65588da9c27cd7960eff
Victor Martinez <knoopx@gmail.com>
2023-11-17 13:26:55 +0100

    Simplify source adding logic

--- src/components/AddSourcePopover.jsx
+++ src/components/AddSourcePopover.jsx
@@ -1,12 +1,6 @@
-import q from "../support/q"
 import { Popover, Spinner } from "."
 import { inject, observer, useLocalStore } from "mobx-react"
 import { MdError } from "react-icons/md"
-import { fetchDoc } from "../support/fetchDoc"
-
-const findFeed = q("link[type='application/rss+xml']@href", (x) =>
-  new URL(x, href).toString(),
-)
 
 export const AddSourcePopover = inject("store")(
   observer(({ store, onDismiss, ...props }) => {
@@ -25,69 +19,12 @@ export const AddSourcePopover = inject("store")(
       },
     }))
 
-    const resolve = async (href) => {
-      console.log(`resolving ${href}`)
-      try {
-        state.update({ isLoading: true, error: false })
-        const doc = await fetchDoc(href)
-        const rootNode = doc.querySelector(":root").nodeName.toLowerCase()
-        if (rootNode == "rss") {
-          store.addSource({
-            title: doc.querySelector("title").textContent.trim(),
-            // link: doc.querySelector("link").textContent.trim(),
-            href,
-            selectors: {
-              item: "item",
-              href: "guid[isPermaLink='true'], link",
-              author: "author",
-              description: "description",
-              publishedAt: "pubDate",
-            },
-          })
-          onDismiss()
-        } else if (rootNode == "feed") {
-          store.addSource({
-            title: doc.querySelector("title").textContent.trim(),
-            // link: doc.querySelector("link").textContent.trim(),
-            href,
-            selectors: {
-              item: "entry",
-              title: "title",
-              href: "link@href",
-              author: "author name",
-              description: "content",
-              publishedAt: "updated",
-            },
-          })
-        } else {
-          const feed = findFeed(doc)
-
-          if (feed) {
-            store.addSource({
-              title: "Loading...",
-              href: feed,
-            })
-            onDismiss()
-          } else {
-            store.addSource({
-              title: "New Source",
-              href,
-              kind: "html",
-            })
-            onDismiss()
-          }
-        }
-      } catch (err) {
-        console.warn(err)
-        state.update({ isLoading: false, error: err })
-      } finally {
-        state.update({ isLoading: false })
-      }
-    }
-
     const onSubmit = (e) => {
       e.preventDefault()
-      resolve(state.httpPrefixed)
+      store.addSource({
+        title: "New Source",
+        href: state.httpPrefixed,
+      })
     }
     return (
       <Popover
